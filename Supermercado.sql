72aac55b15f06fdb61b5b506c4010e5605550e44
Alma2107 <carenaalma2@gmail.com>
2025-10-17 17:49:11 -0300

    Nueva base de datos actualizada

--- Supermercado.sql
+++ Supermercado.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-10-2025 a las 22:57:12
--- Versión del servidor: 10.4.25-MariaDB
--- Versión de PHP: 7.4.30
+-- Tiempo de generación: 17-10-2025 a las 22:37:49
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,13 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `carrito`
+--
+
+CREATE TABLE `carrito` (
+  `Id_Carrito` int(11) NOT NULL,
+  `DNI_Cliente` int(11) NOT NULL,
+  `Id_Direccion` int(11) NOT NULL,
+  `Fecha_Agregado` date NOT NULL,
+  `Estado` varchar(200) NOT NULL,
+  `Costo_Envio` int(11) NOT NULL,
+  `Total_Final` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `categoria`
 --
 
 CREATE TABLE `categoria` (
-  `id_categoria` int(11) NOT NULL,
-  `nombre_categoria` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Id_Categoria` int(11) NOT NULL,
+  `Nombre_Categoria` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -40,8 +56,23 @@ CREATE TABLE `categoria` (
 
 CREATE TABLE `cliente` (
   `id_cliente` int(11) NOT NULL,
-  `id_rol` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id_rol` int(11) NOT NULL,
+  `DNI` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_carrito`
+--
+
+CREATE TABLE `detalle_carrito` (
+  `Id_Detalle_Carrito` int(11) NOT NULL,
+  `Id_Carrito` int(11) NOT NULL,
+  `Id_Opcion_Producto` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `Precio_Unitario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -51,14 +82,29 @@ CREATE TABLE `cliente` (
 
 CREATE TABLE `detalle_venta` (
   `id_detalle_venta` int(11) NOT NULL,
-  `id_venta` int(11) NOT NULL,
-  `id_producto` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `precio_unitario` decimal(10,2) NOT NULL,
-  `IVA` decimal(5,2) NOT NULL,
-  `Subtotal` decimal(10,2) NOT NULL,
-  `Total` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Id_Venta` int(11) NOT NULL,
+  `Id_Producto` int(11) NOT NULL,
+  `Cantidad` int(11) NOT NULL,
+  `Precio_Unitario` int(11) NOT NULL,
+  `IVA` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `direcciones`
+--
+
+CREATE TABLE `direcciones` (
+  `id_direccion` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `nombre_direccion` varchar(200) NOT NULL,
+  `calle_numero` int(11) NOT NULL,
+  `Ciudad` text NOT NULL,
+  `Provincia` text NOT NULL,
+  `Codigo_posta` varchar(200) NOT NULL,
+  `Referencia` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -67,11 +113,25 @@ CREATE TABLE `detalle_venta` (
 --
 
 CREATE TABLE `empleado` (
-  `id_empleado` int(11) NOT NULL,
   `id_rol` int(11) NOT NULL,
-  `cargo` varchar(50) NOT NULL,
-  `fecha_contratacion` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `DNI` int(11) NOT NULL,
+  `Fecha_contratacion` date NOT NULL,
+  `Cargo` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opcion_producto`
+--
+
+CREATE TABLE `opcion_producto` (
+  `Id_Opcion_Producto` int(11) NOT NULL,
+  `Id_Producto` int(11) NOT NULL,
+  `Nombre_Opcion` text NOT NULL,
+  `Precio_Unitario` int(11) NOT NULL,
+  `Stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -80,12 +140,12 @@ CREATE TABLE `empleado` (
 --
 
 CREATE TABLE `producto` (
-  `id_producto` int(11) NOT NULL,
-  `id_categoria` int(11) NOT NULL,
-  `nombre_producto` varchar(100) NOT NULL,
-  `descripcion` text DEFAULT NULL,
-  `stock` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Id_Producto` int(11) NOT NULL,
+  `Id_Categoria` int(11) NOT NULL,
+  `Nombre_Producto` varchar(200) NOT NULL,
+  `Descripcion` varchar(200) NOT NULL,
+  `Stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -95,8 +155,9 @@ CREATE TABLE `producto` (
 
 CREATE TABLE `rol` (
   `id_rol` int(11) NOT NULL,
-  `nombre_rol` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `rol` int(11) NOT NULL,
+  `rol_descripcion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -107,10 +168,10 @@ CREATE TABLE `rol` (
 CREATE TABLE `usuario` (
   `DNI` int(11) NOT NULL,
   `id_rol` int(11) NOT NULL,
-  `nombre_usuario` varchar(50) NOT NULL,
-  `correo` varchar(100) NOT NULL,
-  `contrasena` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `nombre_usuario` varchar(200) NOT NULL,
+  `correo` varchar(200) NOT NULL,
+  `contraseña` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -121,19 +182,27 @@ CREATE TABLE `usuario` (
 CREATE TABLE `venta` (
   `id_venta` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
-  `id_vendedor` int(11) NOT NULL,
+  `id_empleado` int(11) NOT NULL,
   `fecha_venta` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  ADD PRIMARY KEY (`Id_Carrito`),
+  ADD KEY `DNI_Cliente` (`DNI_Cliente`),
+  ADD KEY `Id_Direccion` (`Id_Direccion`);
+
+--
 -- Indices de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`id_categoria`);
+  ADD PRIMARY KEY (`Id_Categoria`);
 
 --
 -- Indices de la tabla `cliente`
@@ -143,26 +212,47 @@ ALTER TABLE `cliente`
   ADD KEY `id_rol` (`id_rol`);
 
 --
+-- Indices de la tabla `detalle_carrito`
+--
+ALTER TABLE `detalle_carrito`
+  ADD PRIMARY KEY (`Id_Detalle_Carrito`),
+  ADD KEY `Id_Carrito` (`Id_Carrito`),
+  ADD KEY `Id_Opcion_Producto` (`Id_Opcion_Producto`);
+
+--
 -- Indices de la tabla `detalle_venta`
 --
 ALTER TABLE `detalle_venta`
   ADD PRIMARY KEY (`id_detalle_venta`),
-  ADD KEY `id_venta` (`id_venta`),
-  ADD KEY `id_producto` (`id_producto`);
+  ADD KEY `Id_Venta` (`Id_Venta`),
+  ADD KEY `Id_Producto` (`Id_Producto`);
+
+--
+-- Indices de la tabla `direcciones`
+--
+ALTER TABLE `direcciones`
+  ADD PRIMARY KEY (`id_direccion`),
+  ADD KEY `id_cliente` (`id_cliente`);
 
 --
 -- Indices de la tabla `empleado`
 --
 ALTER TABLE `empleado`
-  ADD PRIMARY KEY (`id_empleado`),
-  ADD KEY `id_rol` (`id_rol`);
+  ADD UNIQUE KEY `id_rol` (`id_rol`);
+
+--
+-- Indices de la tabla `opcion_producto`
+--
+ALTER TABLE `opcion_producto`
+  ADD PRIMARY KEY (`Id_Opcion_Producto`),
+  ADD KEY `Id_Producto` (`Id_Producto`);
 
 --
 -- Indices de la tabla `producto`
 --
 ALTER TABLE `producto`
-  ADD PRIMARY KEY (`id_producto`),
-  ADD KEY `id_categoria` (`id_categoria`);
+  ADD PRIMARY KEY (`Id_Producto`),
+  ADD KEY `Id_Categoria` (`Id_Categoria`);
 
 --
 -- Indices de la tabla `rol`
@@ -175,7 +265,6 @@ ALTER TABLE `rol`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`DNI`),
-  ADD UNIQUE KEY `correo` (`correo`),
   ADD KEY `id_rol` (`id_rol`);
 
 --
@@ -184,89 +273,17 @@ ALTER TABLE `usuario`
 ALTER TABLE `venta`
   ADD PRIMARY KEY (`id_venta`),
   ADD KEY `id_cliente` (`id_cliente`),
-  ADD KEY `id_vendedor` (`id_vendedor`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `cliente`
---
-ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `detalle_venta`
---
-ALTER TABLE `detalle_venta`
-  MODIFY `id_detalle_venta` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `empleado`
---
-ALTER TABLE `empleado`
-  MODIFY `id_empleado` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `producto`
---
-ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `venta`
---
-ALTER TABLE `venta`
-  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT;
+  ADD KEY `id_empleado` (`id_empleado`);
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `cliente`
---
-ALTER TABLE `cliente`
-  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`);
-
---
--- Filtros para la tabla `detalle_venta`
---
-ALTER TABLE `detalle_venta`
-  ADD CONSTRAINT `detalle_venta_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`),
-  ADD CONSTRAINT `detalle_venta_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`);
-
---
 -- Filtros para la tabla `empleado`
 --
 ALTER TABLE `empleado`
-  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`);
-
---
--- Filtros para la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`);
-
---
--- Filtros para la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`);
-
---
--- Filtros para la tabla `venta`
---
-ALTER TABLE `venta`
-  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
-  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`id_vendedor`) REFERENCES `empleado` (`id_empleado`);
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `cliente` (`id_rol`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
